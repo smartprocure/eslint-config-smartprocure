478dafbf7d14d84e3bf33fdcc649b4face6944da
Daniel Rodríguez <sadasant@gmail.com>
2017-02-10 00:03:48 +0000

    no-unused-vars and no-undef need to be warnings
    so that our apps can comply with eslint:recommemded

--- rules/variables.js
+++ rules/variables.js
@@ -5,6 +5,10 @@ module.exports = {
         'no-catch-shadow':            2,
         'no-shadow-restricted-names': 2,
         'no-shadow':                  1,
-        'no-undef-init':              2
+        'no-undef-init':              2,
+        // Required to extend eslint:recommemded
+        'no-unused-vars':             1,
+        // Required to extend eslint:recommemded because of mocha unit tests
+        'no-undef':                   1
     }
 };
