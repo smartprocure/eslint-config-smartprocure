533c1e1464d12898f4d20a1db315261e15f51c21
Daniel Rodríguez <sadasant@gmail.com>
2017-02-11 14:30:20 +0000

    Added lodash-fp

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ module.exports = {
         './rules/variables'
     ].map(require.resolve)),
     plugins: [
-        'mocha'
+        'mocha',
+        'lodash-fp'
     ],
     parserOptions: {
         ecmaVersion: 7,
@@ -24,15 +25,43 @@ module.exports = {
     },
     rules: {
         // Mocha plugin
-        'mocha/no-exclusive-tests':      'error',
-        'mocha/no-skipped-tests':        'error',
-        'mocha/handle-done-callback':    'error',
-        'mocha/no-global-tests':         'error',
-        'mocha/no-return-and-callback':  'error',
-        'mocha/valid-suite-description': 'error',
-        'mocha/no-sibling-hooks':        'error',
-        'mocha/no-identical-title':      'warn',
+        'mocha/no-exclusive-tests':                  'error',
+        'mocha/no-skipped-tests':                    'error',
+        'mocha/handle-done-callback':                'error',
+        'mocha/no-global-tests':                     'error',
+        'mocha/no-return-and-callback':              'error',
+        'mocha/valid-suite-description':             'error',
+        'mocha/no-sibling-hooks':                    'error',
+        'mocha/no-identical-title':                  'warn',
         // Because of fn().should.be.true and so on
-        'no-unused-expressions': 0
+        'no-unused-expressions':                     0,
+        // Lodash
+        'lodash-fp/consistent-compose':              'error',
+        'lodash-fp/consistent-name':                 [ 'error', '_' ],
+        'lodash-fp/no-argumentless-calls':           'error',
+        'lodash-fp/no-chain':                        'error',
+        'lodash-fp/no-extraneous-args':              'error',
+        'lodash-fp/no-extraneous-function-wrapping': 'error',
+        'lodash-fp/no-extraneous-iteratee-args':     'error',
+        'lodash-fp/no-for-each':                     'error',
+        'lodash-fp/no-partial-of-curried':           'error',
+        'lodash-fp/no-single-composition':           'error',
+        'lodash-fp/no-submodule-destructuring':      'error',
+        'lodash-fp/no-unused-result':                'error',
+        'lodash-fp/prefer-compact':                  'error',
+        'lodash-fp/prefer-composition-grouping':     'error',
+        'lodash-fp/prefer-constant': [
+            'error', {
+                arrowFunctions:                        false
+            }
+        ],
+        'lodash-fp/prefer-flat-map':                 'error',
+        'lodash-fp/prefer-get':                      'error',
+        'lodash-fp/prefer-identity': [
+            'error', {
+                arrowFunctions:                        false
+            }
+        ],
+        'lodash-fp/use-fp':                          'error'
     }
 };
