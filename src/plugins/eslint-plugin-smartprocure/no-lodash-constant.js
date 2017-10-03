d1e51f9a5562c9e9c3c495c78c832d16c1a8f106
Sean Clayton <clayton.sean@gmail.com>
2017-10-03 13:21:00 -0400

    Don't use array of fixers

--- src/plugins/eslint-plugin-smartprocure/no-lodash-constant.js
+++ src/plugins/eslint-plugin-smartprocure/no-lodash-constant.js
@@ -21,7 +21,7 @@ module.exports = {
         ctx.report({
           node,
           message: 'Do not use _.constant',
-          fix: fixer => [fixer.replaceText(node, `() => ${argRaw}`)],
+          fix: fixer => fixer.replaceText(node, `() => ${argRaw}`),
         })
       }
     },
